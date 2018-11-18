5a22bb7a45be875cb59d4c5aba2e455dabf9f107
sumina <suminaxresta@gmail.com>
2018-11-18 08:01:35 +0545

    promise

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 let express = require('express');
+const bodyparser = require('body-parser');
 let app = express();
+let router = express.Router();
 
-// express.static(root, [options]);
+app.use(bodyparser.urlencoded({extended: true}))
+
+app.use(bodyparser.json())
+
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {console.log('successful');
+}).catch(err => {console.log('couldnot connect to db', err);
+                process.exit();
+});
 
 
-app.use(express.static('/images'));
-
-
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
     res.send('port 3000');
 });
 
+require('./app/routes/note.routes.js')(app);
+
 app.listen(3000, () => {console.log('port 3000')
 });
+
